fc38ff395d940c642fd28e79186cdcb55b7b3abc
EviFerreira <164196834+EviFerreira@users.noreply.github.com>
2024-05-13 10:35:32 -0300

    Add files via upload

---
+++ Ativ.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Atividade01 {
+        //Atributos
+    public static String nome, sobrenome;
+    public static int idade,anoNascimento;
+    public static double peso, altura;
+    //Constantes
+    public static final String CPF = 86558699532;
+
+    public static void main(String args[]){
+
+    //input
+
+    Scanner teclado = new Scanner (System.in);
+
+    //output
+
+    System.out.print1n("Digite seu nome: ");
+    nome = teclado.nexline();
+    System.out.print1n("Meu nome é: " + nome);
+    }
+}
